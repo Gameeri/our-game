467134e265183b0a666188829da9ffcf56b2cc9e
dashadem <72491948+dashadem@users.noreply.github.com>
2020-10-18 22:56:01 +0300

    Add files via upload

--- Wall.py
+++ Wall.py
@@ -3,11 +3,13 @@ import os
 
 PLATFORM_WIDTH = 65
 PLATFORM_HEIGHT = 70
-PLATFORM_COLOR = "#FF6262"
 
+wall = image.load('wall2.png')
+wall = transform.scale(wall, (65,70))
 
 class Platform(sprite.Sprite):
     def __init__(self, x, y):
         sprite.Sprite.__init__(self)
-        self.image = Surface((PLATFORM_WIDTH, PLATFORM_HEIGHT))
-        self.rect = Rect(x, y, PLATFORM_WIDTH, PLATFORM_HEIGHT)
+        self.image = wall
+        self.rect = self.image.get_rect()
+        self.rect = (x, y)

a70f599296a1528be03b3612a72744f6a1b3c141
dashadem <72491948+dashadem@users.noreply.github.com>
2020-10-18 23:32:55 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,7 +3,16 @@ import random
 from os import path
 from Map import *
 from Wall import *
-from Player import *
+from Bullet import *
+
+player_stand = pygame.image.load('ded3.png')
+player_right = pygame.image.load('ded_right2.png')
+player_left = pygame.image.load('ded_left2.png')
+player_up = pygame.image.load('ded_up2.png')
+player_stand = pygame.transform.scale(player_stand, (60,55))
+player_right = pygame.transform.scale(player_right, (60,55))
+player_left = pygame.transform.scale(player_left, (60,55))
+player_up = pygame.transform.scale(player_up, (60,55))
 
 MAP = Map(10,10)
 
@@ -17,6 +26,61 @@ BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self,x,y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = player_stand
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+        self.speedx = 0
+        self.speedy = 0
+        self.left, self.right, self.up, self.down = 0, 0, 0, 1
+
+
+    def update(self):
+        self.speedx = 0
+        self.speedy = 0
+        # управление игроком
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_LEFT] or keystate[pygame.K_a]:
+            self.speedx = -8
+            self.image = player_left
+            self.left, self.right, self.up, self.down = 1, 0, 0, 0
+        elif keystate[pygame.K_RIGHT] or keystate[pygame.K_d]:
+            self.speedx = 8
+            self.left, self.right, self.up, self.down = 0, 1, 0, 0
+
+        elif keystate[pygame.K_UP] or keystate[pygame.K_w]:
+            self.speedy = -8
+            self.left, self.right, self.up, self.down = 0, 0, 1, 0
+        elif keystate[pygame.K_DOWN] or keystate[pygame.K_s]:
+            self.speedy = 8
+            self.left, self.right, self.up, self.down = 0, 0, 0, 1
+
+        if self.right == 1:
+            self.image = player_right
+        elif self.left == 1:
+            self.image = player_left
+        elif self.up == 1:
+            self.image = player_up
+        elif self.down == 1:
+            self.image = player_stand
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+
+    def shoot(self):
+        bullet = Bullet(self.rect.centerx, self.rect.top)
+        if(self.left == 1):
+            bullet.direction = 'LEFT'
+        elif (self.right == 1):
+            bullet.direction = 'RIGHT'
+        elif (self.up == 1):
+            bullet.direction = 'UP'
+        elif (self.down == 1):
+            bullet.direction = 'DOWN'
+        all_sprites.add(bullet)
+        bullets.add(bullet)
 
 class Item(pygame.sprite.Sprite):
     def __init__(self):
@@ -38,6 +102,7 @@ background_rect = background.get_rect()
 
 all_sprites = pygame.sprite.Group()
 items = pygame.sprite.Group()
+bullets = pygame.sprite.Group()
 player = Player(WIDTH//2, HEIGHT//2)
 all_sprites.add(player)
 platforms = []
@@ -86,6 +151,8 @@ while running:
             if event.key == pygame.K_e:
                 if (pygame.sprite.spritecollide(player, items, True)):
                     score += 1
+            if event.key == pygame.K_SPACE:
+                player.shoot()
 
     # Обновление
     all_sprites.update()
